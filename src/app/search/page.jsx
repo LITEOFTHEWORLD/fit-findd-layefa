29c792217f8fe423b0c0f5b3e1fc6425bfb60cb6
Code-Sorceress <debbyoziblaze@gmail.com>
2024-09-03 09:30:24 +0100

    still on the description page

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -6,33 +6,35 @@ import Image from "next/image";
 const page = () => {
   return (
     <div className={styles.bg}>
-      <section className={styles.first_part}>
-        <Link href="homepage">
-          <Image
-            src="/assets/arrowback.svg"
-            alt=""
-            width={9.92}
-            height={18.48}
-            className={styles.first_img}
-          />
-        </Link>
-
-        <div className={styles.search_bar}>
-          <Image
-            src="/assets/search.svg"
-            alt="Search Icon"
-            width={16.18}
-            height={16.19}
-          />
-          <input type="text" placeholder="Search" />
-        </div>
-
-        <div className={styles.search_part}>
-          <Link href="search" className={styles.second_btn}>
-            Search
+      <div className={styles.wrapper}>
+        <section className={styles.first_part}>
+          <Link href="homepage">
+            <Image
+              src="/assets/arrowback.svg"
+              alt=""
+              width={9.92}
+              height={18.48}
+              className={styles.first_img}
+            />
           </Link>
-        </div>
-      </section>
+
+          <div className={styles.search_bar}>
+            <Image
+              src="/assets/search.svg"
+              alt="Search Icon"
+              width={16.18}
+              height={16.19}
+            />
+            <input type="text" placeholder="Search" />
+          </div>
+
+          <div className={styles.search_part}>
+            <Link href="search" className={styles.second_btn}>
+              Search
+            </Link>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
